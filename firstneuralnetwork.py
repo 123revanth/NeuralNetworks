c8172461f19e3e41d7b39c988291fae5edc5709c
Revanth jallipalli <81552258+123revanth@users.noreply.github.com>
2023-07-15 09:38:26 +0530

    Create firstneuralnetwork.py

---
+++ firstneuralnetwork.py
@@ -0,0 +1,46 @@
+import numpy as np
+from scipy.special import expit as activation_function
+from scipy.stats import truncnorm
+
+def truncated_normal(mean=0, sd=1, low=0, upp=10):
+    return truncnorm(
+        (low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
+  
+class Nnetwork:
+
+    def __init__(self, 
+                 no_of_in_nodes, 
+                 no_of_out_nodes, 
+                 no_of_hidden_nodes,
+                 learning_rate):
+        self.no_of_in_nodes = no_of_in_nodes
+        self.no_of_out_nodes = no_of_out_nodes
+        self.no_of_hidden_nodes = no_of_hidden_nodes
+        self.learning_rate = learning_rate 
+        self.create_weight_matrices()
+        
+    def create_weight_matrices(self):
+        rad = 1 / np.sqrt(self.no_of_in_nodes)
+        X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
+        self.weights_in_hidden = X.rvs((self.no_of_hidden_nodes, 
+                                       self.no_of_in_nodes))
+        rad = 1 / np.sqrt(self.no_of_hidden_nodes)
+        X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
+        self.weights_hidden_out = X.rvs((self.no_of_out_nodes, 
+                                        self.no_of_hidden_nodes))
+
+    def train(self, input_vector, target_vector):
+        pass 
+            
+    def run(self, input_vector):
+        input_vector = np.array(input_vector, ndmin=2).T
+        input_hidden = activation_function(self.weights_in_hidden @   input_vector)
+        output_vector = activation_function(self.weights_hidden_out @ input_hidden)
+        return output_vector 
+     
+simple_network = Nnetwork(no_of_in_nodes=2, 
+                               no_of_out_nodes=2, 
+                               no_of_hidden_nodes=4,
+                               learning_rate=0.6)
+
+simple_network.run([(3, 4)])
